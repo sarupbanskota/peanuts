b40104e00403596476bcacf88844822f92f4d1c0
Sarup Banskota <sbanskota08@gmail.com>
2015-02-08 01:26:12 +0530

    Some interactions, add icon

--- banq/index.js
+++ banq/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	$(".dropdown-list").hide();
 	$(".more").hide();
 	$("section.landing > div > h2").click(function(){
 		$("h2").hide();
@@ -7,6 +8,25 @@ $(document).ready(function(){
 	$("section.table > div.loader > h2").click(function(){
 		$(this).closest("section").next("section.more").show();
 		$(this).hide();
+	});
+	$("header > h1").click(function(){
+		$(this).next(".dropdown-list").toggle();
+	});
+	$(".dropdown-list > h2").click(function(){
+		var dropped_value = $(this).html();
+		$(this).html($(this).parent().prev("h1").html());
+		$(this).parent().prev("h1").html(dropped_value);
+		$(this).parent().hide();
+
+		//$(".graph").hide();
+		//$("#" + dropped_value.replace(" ", "")).show();
+
+		//put this in the header
+		//put header into the h2 spot
+		//hide whats currently in graph
+		//put the graph corresponding to that id
+		//hide whats currently in the table
+		//put table corresponding to id
 	});
 
 });
